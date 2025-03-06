d3eb3b5e4260da38dd08e48fd3233213dbd939fc
Mahtab-Hossain <mahtab.hossain.bhuiyan@gmail.com>
2025-03-07 02:00:45 +0600

    added logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,9 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">Blogy</a>
+            <a class="navbar-brand" href="{{ url('/') }}">
+                <img src="{{ asset('logo.jpg') }}" alt="Logo" style="height: 40px;">
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
